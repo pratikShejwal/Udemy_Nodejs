3af9ed0e493dc528a2768bdb9638c2e748ee9286
pratikShejwal <shejwalp147@gmail.com>
2026-04-01 01:06:18 +0530

    basic http server

---
+++ http-server/server.js
@@ -0,0 +1,28 @@
+const http = require('node:http')
+
+const server = http.createServer((req,res)=>{
+    console.log(Date.now());
+    console.log(req.headers);
+
+    //
+    switch (req.url) {
+
+        case '/':
+             res.writeHead(200)
+              res.end('Home-Page')
+            break;
+         case '/contact':
+            res.end('Contact-Page')
+            break;
+        default:
+            res.end('Error')
+              res.writeHead(400)
+              
+            break;
+    }
+    res.end('Ok')
+})
+
+server.listen(8000,()=>{
+    console.log('running'); 
+})
