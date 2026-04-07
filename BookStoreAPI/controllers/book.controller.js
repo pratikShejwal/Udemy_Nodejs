1ba6b51312f33ae59005af22021498fe60f4c696
pratikShejwal <shejwalp147@gmail.com>
2026-04-08 00:09:27 +0530

    indexes on title column

--- BookStoreAPI/controllers/book.controller.js
+++ BookStoreAPI/controllers/book.controller.js
@@ -1,16 +1,26 @@
 const {books} = require('../db/book')
 const booksTable = require('../models/book.model')
 const db = require('../db')
-const {eq} = require('drizzle-orm')
-
+const {eq, ilike} = require('drizzle-orm')
+const {sql} = require('drizzle-orm')
 exports.getAllBooks = async (req,res)=>{
+    const search = req.query.search
+    if (search) {
+        // const books = await db.select().from(booksTable).where(ilike(booksTable.title, `%${search}%`))
+        const books = await db.select().from(booksTable)
+        .where(sql`to_tsvector('english', ${booksTable.title}) @@ to_tsquery('english', ${search})`);
+        
+        return res.json(books)
+    }
+    console.log(search);
+    
     const books = await db.select().from(booksTable)
     return res.json(books)
 }
 
 exports.getBookById = async (req,res)=>{
     const id = req.params.id
-    const book = db.select().from(booksTable).where((table)=>eq(table.id,id)).limit(1)
+    const book = await db.select().from(booksTable).where((table)=>eq(table.id,id)).limit(1)
 
     if(!book){
         return res.status(404).json({error:'Book not found'})
