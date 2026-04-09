3b539b13662128f08127c824b452249e5e5beb60
pratikShejwal <shejwalp147@gmail.com>
2026-04-10 01:02:25 +0530

    basics of auth

---
+++ authentication_01/index.js
@@ -0,0 +1,37 @@
+import express from 'express'
+const app = express()
+const PORT = 8000
+
+app.use(express.json())
+const DIARY = {};
+const EMAILS = new Set()
+
+app.post('/signup',(req,res)=>{
+    const {name, email, password} = req.body
+
+    if (EMAILS.has(email)) {
+        return res.status(400).json({error:'email taken'})
+    }
+
+    const token = `${Date.now()}`
+    
+    DIARY[token] = {name,email,password}
+    EMAILS.add(email)
+          return res.status(201).json({message:'user created',token})
+})
+
+app.post('/me',(req,res)=>{
+    const {token} = req.body
+    if (!token) {
+        return res.status(404).json({error:'missing token'})
+    }
+    if (!(token in DIARY)){
+        return res.status(400).json({error:'not token'})
+    }
+    const entry = DIARY[token]
+    return res.json({data: entry})
+})
+app.listen(PORT,()=>{
+    console.log('running on 8k');
+    
+})
